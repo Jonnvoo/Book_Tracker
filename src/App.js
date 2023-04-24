4f489a67104eebde184e8489949a6e11b6827405
Jonnvoo <Jonathanvu2065@gmail.com>
2023-04-24 15:51:18 -0700

    added ids for each book created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import BookCreate from './components/BookCreate';
+
+function App(){
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updatedBooks = [...books, {id:123, title}];
+        setBooks(updatedBooks);
+    };
+    return <div>
+        <BookCreate onSubmit={createBook}/>
+    </div>;
+}
+
+export default App;
